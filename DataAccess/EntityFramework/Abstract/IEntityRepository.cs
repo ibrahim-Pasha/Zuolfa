9453380891b4fe4bb13305c854b83800bc755903
ibrahim issa basha <94835822+PASHA011@users.noreply.github.com>
2025-05-15 10:50:08 +0300

    Reapply "Repositories and dbContext file"
    This reverts commit 5f8c194b9c7e7572a63ec2e1ca285407ace9b812.

---
+++ DataAccess/EntityFramework/Abstract/IEntityRepository.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System.Linq.Expressions;
+
+namespace Data_Access.EntityFramework.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(Guid id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveAsync();
+    }
+}
---
+++ DataAccess/EntityFramework/Concrete/TeacherRepository.cs
@@ -0,0 +1,34 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using Data_Access.EntityFramework.Abstract;
+using Data_Access.EntityFramework;
+
+
+namespace ZuolfaWebApi.Data_Access.Concrete
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        private readonly ZuolfaContext _dbContext;
+
+        public TeacherRepository(ZuolfaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<IEnumerable<Teacher>> GetAllAsync()
+         => await _dbContext.Teachers.ToListAsync();
+
+        public async Task<Teacher> GetByIdAsync(Guid id)
+            => await _dbContext.Teachers.FindAsync(id);
+
+        public async Task AddAsync(Teacher teacher)
+            => await _dbContext.Teachers.AddAsync(teacher);
+
+        public void Update(Teacher teacher)
+            => _dbContext.Teachers.Update(teacher);
+
+        public void Delete(Teacher teacher)
+            => _dbContext.Teachers.Remove(teacher);
+        public async Task SaveAsync()
+            => await _dbContext.SaveChangesAsync();
+    }
+}
